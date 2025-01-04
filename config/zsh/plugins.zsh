c80e1b8fa542cb9a64d7d11fc3ed5a853dbe3085
hanna <hanna@hanna.lol>
2025-01-04 17:02:26 -0500

    chore: add config directory and files

---
+++ config/zsh/plugins.zsh
@@ -0,0 +1,4 @@
+fpath+=( $HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
+source $HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( $HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source $HOME/.local/share/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
